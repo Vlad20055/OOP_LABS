e1a813e9bb235d9d292d250db6aa511b3400422d
Vlad20055 <maksimenkovvlad111@mail.ru>
2025-03-29 15:56:22 +0300

    Add class diagram

--- Lab1/Lab1/Domain/Users/User.cs
+++ Lab1/Lab1/Domain/Users/User.cs
@@ -11,7 +11,7 @@ namespace Lab1.Domain.Users
         Operator
     }
 
-    internal class User
+    internal abstract class User
     {
         public UserRole Role { get; set; } // who is the user (Client, Manager, ...)
         public required string Login { get; set; }
